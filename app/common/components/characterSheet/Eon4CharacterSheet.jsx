f9ef0dc7f412df811847cc5df925c09b158b8a71
Sebastian Lindström <sebastian.lindstrom@gmail.com>
2017-06-01 17:13:00 +0200

    Sheet layout fixes

--- app/common/components/characterSheet/Eon4CharacterSheet.jsx
+++ app/common/components/characterSheet/Eon4CharacterSheet.jsx
@@ -11,87 +11,207 @@ import {
 
 import Attribute from './Attribute'
 import Section from './Section'
+import Column from './Column'
 
 const Eon4BaseAttributeInput = 
     ({ name }) => <Attribute withBonus name={name} valueSuffix="T6" bonusPrefix="+"maxBonus = {3} />
 
 const Eon4CharacterSheet = () => (
     <div>
-        <Section title="Grundinfo">
-            <div>
-                <TextField hintText="Namn"/>
-                <TextField hintText="Folkslag"/>
-                <TextField hintText="Kön"/>
-            </div>
-            <div>
-                <TextField hintText="Arketyp"/>
-                <TextField hintText="Miljo"/>
-                <TextField hintText="Bakgrund"/>
-            </div>
-        </Section>
-        <Section title="Grundegenskaper">
-            <Table selectable={false}>
-                <TableBody displayRowCheckbox={false}>
-                <TableRow>
-                    <TableRowColumn>
-                        <Eon4BaseAttributeInput name="Styrka"/>
-                    </TableRowColumn>
-                </TableRow>
-                <TableRow>
-                    <TableRowColumn>
-                        <Eon4BaseAttributeInput name="Talighet"/>
-                    </TableRowColumn>
-                </TableRow>
-                <TableRow>
-                    <TableRowColumn>
-                        <Eon4BaseAttributeInput name="Rorlighet"/>
-                    </TableRowColumn>
-                </TableRow>
-                <TableRow>
-                    <TableRowColumn>
-                        <Eon4BaseAttributeInput name="Uppfattning"/>
-                    </TableRowColumn>
-                </TableRow>
-                </TableBody>
-            </Table>
-            
-            <Table selectable={false}>
-                <TableBody displayRowCheckbox={false}>
-                <TableRow>
-                    <TableRowColumn>
-                        <Eon4BaseAttributeInput name="Vilja"/>
-                    </TableRowColumn>
-                </TableRow>
-                <TableRow>
-                    <TableRowColumn>
-                        <Eon4BaseAttributeInput name="Psyke"/>
-                    </TableRowColumn>
-                </TableRow>
-                <TableRow>
-                    <TableRowColumn>
-                        <Eon4BaseAttributeInput name="Visdom"/>
-                    </TableRowColumn>
-                </TableRow>
-                <TableRow>
-                    <TableRowColumn>
-                        <Eon4BaseAttributeInput name="Utstralning"/>
-                    </TableRowColumn>
-                </TableRow>
-                </TableBody>
-            </Table>
-        </Section>
-        <Section title="Harledda Varden">
-            <div>
-                <TextField hintText="Namn"/>
-                <TextField hintText="Folkslag"/>
-                <TextField hintText="Kön"/>
-            </div>
-            <div>
-                <TextField hintText="Arketyp"/>
-                <TextField hintText="Miljo"/>
-                <TextField hintText="Bakgrund"/>
-            </div>
-        </Section>
+        <Column>
+            <Section title="Grundinfo">
+                <div>
+                    <TextField floatingLabelText="Namn"/>
+                    <TextField floatingLabelText="Folkslag"/>
+                    <TextField floatingLabelText="Kön"/>
+                </div>
+                <div>
+                    <TextField floatingLabelText="Arketyp"/>
+                    <TextField floatingLabelText="Miljo"/>
+                    <TextField floatingLabelText="Bakgrund"/>
+                </div>
+                <div>
+                    <TextField floatingLabelText="Namn"/>
+                    <TextField floatingLabelText="Folkslag"/>
+                    <TextField floatingLabelText="Kön"/>
+                </div>
+                <div>
+                    <TextField floatingLabelText="Arketyp"/>
+                    <TextField floatingLabelText="Miljo"/>
+                    <TextField floatingLabelText="Bakgrund"/>
+                </div>
+            </Section>
+        </Column>
+        <Column>
+            <Section title="Karaktarsdrag">
+                <div>
+                    <TextField floatingLabelText="Primart"/>
+                    <Table selectable={false}>
+                        <TableBody displayRowCheckbox={false}>
+                        <TableRow>
+                            <TableRowColumn>
+                                <Eon4BaseAttributeInput name="Styrka"/>
+                            </TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>
+                                <Eon4BaseAttributeInput name="Talighet"/>
+                            </TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>
+                                <Eon4BaseAttributeInput name="Rorlighet"/>
+                            </TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>
+                                <Eon4BaseAttributeInput name="Uppfattning"/>
+                            </TableRowColumn>
+                        </TableRow>
+                        </TableBody>
+                    </Table>
+                </div>
+                <div>
+                    <TextField floatingLabelText="Sekundart"/>
+                    <Table selectable={false}>
+                        <TableBody displayRowCheckbox={false}>
+                        <TableRow>
+                            <TableRowColumn>
+                                <Eon4BaseAttributeInput name="Styrka"/>
+                            </TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>
+                                <Eon4BaseAttributeInput name="Talighet"/>
+                            </TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>
+                                <Eon4BaseAttributeInput name="Rorlighet"/>
+                            </TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>
+                                <Eon4BaseAttributeInput name="Uppfattning"/>
+                            </TableRowColumn>
+                        </TableRow>
+                        </TableBody>
+                    </Table>
+                </div>
+            </Section>
+            <Section title="Fokus">
+                <div>
+                    <TextField floatingLabelText="Namn"/>
+                    <TextField floatingLabelText="Folkslag"/>
+                    <TextField floatingLabelText="Kön"/>
+                </div>
+                <div>
+                    <TextField floatingLabelText="Arketyp"/>
+                    <TextField floatingLabelText="Miljo"/>
+                    <TextField floatingLabelText="Bakgrund"/>
+                </div>
+            </Section>
+        </Column>
+        <Column>
+            <Section half title="Grundegenskaper">
+                <Table selectable={false}>
+                    <TableBody displayRowCheckbox={false}>
+                    <TableRow>
+                        <TableRowColumn>
+                            <Eon4BaseAttributeInput name="Styrka"/>
+                        </TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                        <TableRowColumn>
+                            <Eon4BaseAttributeInput name="Talighet"/>
+                        </TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                        <TableRowColumn>
+                            <Eon4BaseAttributeInput name="Rorlighet"/>
+                        </TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                        <TableRowColumn>
+                            <Eon4BaseAttributeInput name="Uppfattning"/>
+                        </TableRowColumn>
+                    </TableRow>
+                    </TableBody>
+                </Table>
+                
+                <Table selectable={false}>
+                    <TableBody displayRowCheckbox={false}>
+                    <TableRow>
+                        <TableRowColumn>
+                            <Eon4BaseAttributeInput name="Vilja"/>
+                        </TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                        <TableRowColumn>
+                            <Eon4BaseAttributeInput name="Psyke"/>
+                        </TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                        <TableRowColumn>
+                            <Eon4BaseAttributeInput name="Visdom"/>
+                        </TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                        <TableRowColumn>
+                            <Eon4BaseAttributeInput name="Utstralning"/>
+                        </TableRowColumn>
+                    </TableRow>
+                    </TableBody>
+                </Table>
+            </Section>
+            <Section half title="Harledda Varden">
+                
+                <Table selectable={false}>
+                    <TableBody displayRowCheckbox={false}>
+                    <TableRow>
+                        <TableRowColumn>
+                            <Eon4BaseAttributeInput name="Forflyttning"/>
+                        </TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                        <TableRowColumn>
+                            <Eon4BaseAttributeInput name="Intryck"/>
+                        </TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                        <TableRowColumn>
+                            <Eon4BaseAttributeInput name="Kroppsbyggnad"/>
+                        </TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                        <TableRowColumn>
+                            <Eon4BaseAttributeInput name="Reaktion"/>
+                        </TableRowColumn>
+                    </TableRow>
+                    </TableBody>
+                </Table>
+
+                <Table selectable={false}>
+                    <TableBody displayRowCheckbox={false}>
+                    <TableRow>
+                        <TableRowColumn>
+                            <Eon4BaseAttributeInput name="Sjalvkontroll"/>
+                        </TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                        <TableRowColumn>
+                            <Eon4BaseAttributeInput name="Vaksamhet"/>
+                        </TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                        <TableRowColumn>
+                            <Eon4BaseAttributeInput name="Grundskada"/>
+                        </TableRowColumn>
+                    </TableRow>
+                    </TableBody>
+                </Table>
+            </Section>
+        </Column>
     </div>
 )
 
--- app/common/components/characterSheet/Section.jsx
+++ app/common/components/characterSheet/Section.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import styles from './section.scss'
 
-const Section = ({ title, children }) => (
-  <Card style={{ marginTop: '6px' }}>
+const Section = ({ title, children, half }) => (
+  <Card style={{ marginTop: '6px', flex: (half ? '2' : '1')}}>
     <CardHeader title={title} />
     <CardText children={children} style={{display:'flex'}}/>
   </Card>
